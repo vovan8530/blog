9536580c22d94bdd1f242e6a676a9a42c6f20d38
vovan <you@example.com>
2019-07-18 19:14:24 +0300

    add simple user

--- laravel/database/seeds/UsersTableSeeder.php
+++ laravel/database/seeds/UsersTableSeeder.php
@@ -12,6 +12,11 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        factory(User::class)->create([
+            'email' => 'test@test.com',
+            'password' => bcrypt(123456),
+
+        ]);
         factory(User::class,10)->create();
     }
 }
