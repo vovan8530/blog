0086f8377625a9108fd489ada3517ddcc3653a00
vovan <you@example.com>
2019-07-19 14:12:48 +0300

    add relation to Post

--- laravel/app/Models/Post.php
+++ laravel/app/Models/Post.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Post extends Model
 {
     protected $keyType = 'str';
     protected $primaryKey='slug';
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
