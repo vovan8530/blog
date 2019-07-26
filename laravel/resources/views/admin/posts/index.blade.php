a2d626d66cf05fad8b3ddb39c0dcae953ad02910
vovan <you@example.com>
2019-07-26 18:32:56 +0300

    add Search form

--- laravel/resources/views/admin/posts/index.blade.php
+++ laravel/resources/views/admin/posts/index.blade.php
@@ -8,6 +8,19 @@
             </div>
         </div>
         <div class="row">
+            <form>
+                <div class="form-row align-items-center">
+                    <div class="col-auto">
+                        <input type="text" class="form-control mb-2" id="inlineFormInput" name="search"  placeholder="Enter search phrase">
+                    </div>
+
+                    <div class="col-auto">
+                        <button type="submit" class="btn btn-primary mb-2">Search</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="row">
             <table class="table table-striped">
                 <thead>
                 <tr>
