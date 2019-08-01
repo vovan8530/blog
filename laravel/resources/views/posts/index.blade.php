f8d80b437ddb7a82ffb677a3d77984ed64936683
vovan <you@example.com>
2019-08-01 16:17:32 +0300

    create show post for user

--- laravel/resources/views/posts/index.blade.php
+++ laravel/resources/views/posts/index.blade.php
@@ -14,7 +14,7 @@
                     <img src="..." class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">{{$post->title}}</h5>
-                        <p class="card-text">{{$post->body}}</p>
+                        <p class="card-text">{!! $post->body !!}</p>
                         <p class="card-text">
                             <a href="{{route('posts.show',[$post->slug])}}" class="btn btn-default">Show more</a>
                         </p>
--- laravel/resources/views/posts/show.blade.php
+++ laravel/resources/views/posts/show.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>{{$post->title}}</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                {!! $post->body !!}
+            </div>
+        </div>
+    </div>
 @endsection
