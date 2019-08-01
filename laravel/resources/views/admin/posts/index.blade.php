0e9f6dd5f198ad69fe16ce6ef5580cbda619fefc
vovan <you@example.com>
2019-08-01 10:06:37 +0300

    fix pagination

--- laravel/resources/views/admin/posts/index.blade.php
+++ laravel/resources/views/admin/posts/index.blade.php
@@ -35,7 +35,7 @@
                 <tbody>
                 @foreach($posts as $post)
                     <tr>
-                        <th scope="row">{{$loop->index+1+($posts->count() * ($posts->currentPage()-1))}}</th>
+                        <th scope="row">{{$loop->index+1+($posts->perPage() * ($posts->currentPage()-1))}}</th>
                         <td>{{$post->title}}</td>
                         <td>{{$post->published_status}}</td>
                         <td>{{$post->user->name}}</td>
