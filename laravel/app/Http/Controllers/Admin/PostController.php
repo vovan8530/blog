eddcfd22a2e3e0ce0a1880f906ed637ebeb3c44a
vovan <you@example.com>
2019-08-01 12:26:38 +0300

    add admin cteate method

--- laravel/app/Http/Controllers/Admin/PostController.php
+++ laravel/app/Http/Controllers/Admin/PostController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\View\View;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -22,24 +23,27 @@ class PostController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @return View
      */
-    public function create()
+    public function create():View
     {
-        //
+        return view('admin.posts.create',[
+            'published_status' => config('config.models.published_status'),
+        ]);
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Post $post
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request)
+    public function store(Request $request, Post $post)
     {
-        //
+        $post->create(array_merge($request->all(),[
+            'slug' => Str::slug($request->title),
+            'user_id'=> auth()->user()->id,
+            ]));
+        return redirect()->route('admin.posts.index');
     }
 
 
--- laravel/resources/views/admin/posts/create.blade.php
+++ laravel/resources/views/admin/posts/create.blade.php
@@ -1,1 +1,42 @@
-<?php
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <form action="{{route('admin.posts.store')}}" method="post">
+                    @csrf
+                    <div class="form-group row">
+                        <label for="title" class="col-sm-2 col-form-label">Title</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="title" id="title">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="title" class="col-sm-2 col-form-label">Body</label>
+                        <div class="col-sm-10">
+                            <textarea class="form-control" id="body" name="body" rows="10"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="status" class="col-sm-2 col-form-label">Status</label>
+                        <div class="col-sm-10">
+                            <select class="form-control" id="status" name="published_status">
+                                @foreach($published_status as $status)
+                                    <option>{{$status}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Create</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
